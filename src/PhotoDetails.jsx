ffbe115074504c5abc12acf48db281a3cee0769a
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-11-08 01:27:03 +0600

    navigate added

--- src/PhotoDetails.jsx
+++ src/PhotoDetails.jsx
@@ -1,12 +1,17 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PhotoDetails = () => {
     const photoDetails = useLoaderData();
+    const navigate = useNavigate();
     console.log(photoDetails);
+    const handleGoBack = () => {
+        navigate(-1);
+    };
     return (
         <div className="text-center border border-[red] rounded-full m-4 p-4">
             <h2>{photoDetails.title}</h2>
             <img className="mx-auto" src={photoDetails.url} alt="" />
+            <button onClick={handleGoBack} className="btn btn-info">Go back</button>
             
         </div>
     );
--- src/components/home/Photo.jsx
+++ src/components/home/Photo.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Photo = ({photo}) => {
     const {id, thumbnailUrl} = photo;
+    const navigate = useNavigate();
+    const handleGoback = () => {
+        navigate(-1);
+    };
     return (
         <div className="border border-[green] text-center m-3 p-3 mx-auto">
             <h3>{id}</h3>
             <img src={thumbnailUrl} alt="" />
             <Link to={`/photo/${id}`}><button className="btn">Details</button></Link>
+            <button onClick={handleGoback} className="btn btn-warning">Go back</button>
         </div>
     );
 };
